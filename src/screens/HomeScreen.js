42d14e850e6398ba2173bbdba5b7428721f467fb
Kenny Jung <dukyoung01@naver.com>
2021-02-13 15:14:09 -0500

    Set up home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text>Home page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
